1ac9354f2ab2ca9d52848a79ad8db44e215caa11
40823224 <40823224@gm.nfu.edu.tw>
2020-01-04 18:23:17 +0800

    更新頁面

--- downloads/dart_ex/flag/flag.dart
+++ downloads/dart_ex/flag/flag.dart
@@ -5,11 +5,17 @@ CanvasElement canvas;
 CanvasRenderingContext2D ctx;
 int flag_w = 300;
 int flag_h = 200;
+
+num flag_w_uk = 300;
+num flag_h_uk = 150;
+
 num circle_x = flag_w / 4;
 num circle_y = flag_h / 4;
 
+num circle_x_jan = flag_w / 2;
+num circle_y_jan = flag_h / 2;
 
- fivePointStar(x, y, r, solid, theta, color) {
+fivePointStar(x, y, r, solid, theta, color) {
   List outerPoints = [];
   List innerPoints = [];
   var degree = Math.pi / 180;
@@ -51,52 +57,34 @@ num circle_y = flag_h / 4;
   }
 }
 
-
 void main() {
   canvas = querySelector('#canvas');
   ctx = canvas.getContext('2d');
-  
- 
 
-  drawCuba(ctx);
-  querySelector("#cuba").onClick.listen((e) => drawCuba(ctx));
-  querySelector("#china").onClick.listen((e) => drawCHINA(ctx));
-  querySelector("#button").onClick.listen((e) => clearCanvas());
-  querySelector("#San").onClick.listen((e) => drawSAN(ctx));
+  drawROC(ctx);
+  querySelector("#roc").onClick.listen((e) => drawROC(ctx));
   querySelector("#usa").onClick.listen((e) => drawUSA(ctx));
+  querySelector("#jpn").onClick.listen((e) => drawJPN(ctx));
+  querySelector("#fra").onClick.listen((e) => drawFRA(ctx));
+  querySelector("#rus").onClick.listen((e) => drawRUS(ctx));
   querySelector("#uk").onClick.listen((e) => drawUK(ctx));
-  querySelector("#canada").onClick.listen((e) => drawCanada(ctx));
-}
+  querySelector("#deu").onClick.listen((e) => drawDEU(ctx));
+  querySelector("#nld").onClick.listen((e) => drawNLD(ctx));
   
-  drawStar(cx, cy, spikes, outerRadius, innerRadius) {
-    var rot = Math.pi / 2 * 3;
-    var x = cx;
-    var y = cy;
-    var step = Math.pi / spikes;
-
-    ctx.fillStyle = '#fff';
-    ctx.beginPath();
-    ctx.moveTo(cx, cy - outerRadius);
-    for (int i = 0; i < spikes; i++) {
-        x = cx + Math.cos(rot) * outerRadius;
-        y = cy + Math.sin(rot) * outerRadius;
-        ctx.lineTo(x, y);
-        rot += step;
-
-        x = cx + Math.cos(rot) * innerRadius;
-        y = cy + Math.sin(rot) * innerRadius;
-        ctx.lineTo(x, y);
-        rot += step;
-    }
-    ctx.lineTo(cx, cy - outerRadius);
-    ctx.closePath();
-    ctx.lineWidth=5;
-    ctx.strokeStyle='white';
-    ctx.stroke();
-    ctx.fillStyle='white';
-    ctx.fill();
-
+  querySelector("#cub").onClick.listen((e) => drawCUB(ctx));
+  querySelector("#china").onClick.listen((e) => drawCHI(ctx));
+  querySelector("#San").onClick.listen((e) => drawSAN(ctx));
+  querySelector("#can").onClick.listen((e) => drawCAN(ctx));
+  querySelector("#button").onClick.listen((e) => clearCanvas());
 }
+
+void drawCHINA(ctx){
+  //紅底
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  
+  
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
   drawStarY(cx, cy, spikes, outerRadius, innerRadius) {
     var rot = Math.pi / 2 * 3;
     var x = cx;
@@ -186,19 +174,14 @@ void main() {
     ctx.fill();
 
 }
-
-void drawCHINA(ctx){
-  //紅底
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(255, 0, 0)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
   drawStarY(50, 50, 5, 20, 10);  
   drawStarYT1(120, 25, 5, 5, 2.5); 
   drawStarYT(135, 50, 5, 5, 2.5); 
   drawStarY(135, 75, 5, 5, 2.5); 
   drawStarYT1(120, 100, 5, 5, 2.5); 
-} 
-  void drawCuba(ctx){
+}
+
+void drawCuba(ctx){
   ctx.clearRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = 'rgb(0, 0, 140)';
   ctx.fillRect(0, 0, flag_w, flag_h);
@@ -212,9 +195,40 @@ void drawCHINA(ctx){
   ctx.lineTo(0,200);
   ctx.fillStyle="rgb(255, 0, 0)";
   ctx.fill();  
+  drawStar(cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = Math.pi / 2 * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.pi / spikes;
+
+    ctx.fillStyle = '#fff';
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+    for (int i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+    }
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.lineWidth=5;
+    ctx.strokeStyle='white';
+    ctx.stroke();
+    ctx.fillStyle='white';
+    ctx.fill();
   drawStar(50, 100, 5, 20, 10);
-  
+    
+    
+
 }
+  
+
 void drawSAN(ctx){
   ctx.clearRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = 'rgb(0, 200, 25)';
@@ -262,114 +276,7 @@ void drawSAN(ctx){
   fivePointStar(a, b, c, solid1, theta1, color1);
 }
 
-void drawUSA(ctx){
-  //紅底
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(255, 0, 0)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  //白線
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(0, 15.3, flag_w, flag_h/13);
-  ctx.fillRect(0, 45.9, flag_w, flag_h/13);
-  ctx.fillRect(0, 75.5, flag_w, flag_h/13);
-  ctx.fillRect(0, 106.1, flag_w, flag_h/13);
-  ctx.fillRect(0, 136.7, flag_w, flag_h/13);
-  ctx.fillRect(0, 167.3, flag_w, flag_h/13);
-  //籃底
-  ctx.fillStyle = 'rgb(0, 0, 140)';
-  ctx.fillRect(0, 0, 150,106.1 );
-  //星星
-  drawStar(14, 17, 5, 5, 2.5);
-  drawStar(38, 17, 5, 5, 2.5);
-  drawStar(62, 17, 5, 5, 2.5);
-  drawStar(88, 17, 5, 5, 2.5);
-  drawStar(112, 17, 5, 5, 2.5);
-  drawStar(136, 17, 5, 5, 2.5);
-  
-  drawStar(25, 36, 5, 5, 2.5);
-  drawStar(50, 36, 5, 5, 2.5);
-  drawStar(75, 36, 5, 5, 2.5);
-  drawStar(100, 36, 5, 5, 2.5);
-  drawStar(125, 36, 5, 5, 2.5);
-  
-  drawStar(14, 54, 5, 5, 2.5);
-  drawStar(38, 54, 5, 5, 2.5);
-  drawStar(62, 54, 5, 5, 2.5);
-  drawStar(88, 54, 5, 5, 2.5);
-  drawStar(112, 54, 5, 5, 2.5);
-  drawStar(136, 54, 5, 5, 2.5);
-  
-  drawStar(25, 71, 5, 5, 2.5);
-  drawStar(50, 71, 5, 5, 2.5);
-  drawStar(75, 71, 5, 5, 2.5);
-  drawStar(100, 71, 5, 5, 2.5);
-  drawStar(125, 71, 5, 5, 2.5);
-  
-  drawStar(14, 88, 5, 5, 2.5);
-  drawStar(38, 88, 5, 5, 2.5);
-  drawStar(62, 88, 5, 5, 2.5);
-  drawStar(88, 88, 5, 5, 2.5);
-  drawStar(112, 88, 5, 5, 2.5);
-  drawStar(136, 88, 5, 5, 2.5);
-} 
-
-void drawUK(ctx){
-  ctx.clearRect(0, 0, flag_w, flag_h);
-  ctx.fillStyle = 'rgb(0, 0, 140)';
-  ctx.fillRect(0, 0, flag_w, flag_h);
-  ctx.strokeStyle="#fff";
-  ctx.lineWidth = 40;
-  ctx.beginPath();
-  ctx.moveTo(0,0);
-  ctx.lineTo(300,200);
-  ctx.moveTo(0,200);
-  ctx.lineTo(300,0);
-  ctx.stroke();
-  ctx.strokeStyle="#c9072a";
-  ctx.lineWidth = 20;
-  ctx.beginPath();
-  ctx.moveTo(0,205);
-  ctx.lineTo(150,105);
-  ctx.stroke();
-  ctx.strokeStyle="#c9072a";
-  ctx.lineWidth = 20;
-  ctx.beginPath();
-  ctx.moveTo(150,95);
-  ctx.lineTo(300,-5);
-  ctx.stroke();
-  ctx.strokeStyle="#c9072a";
-  ctx.lineWidth = 20;
-  ctx.beginPath();
-  ctx.moveTo(160,100);
-  ctx.lineTo(305,200);
-  ctx.stroke();
-  ctx.strokeStyle="#c9072a";
-  ctx.lineWidth = 20;
-  ctx.beginPath();
-  ctx.moveTo(140,100);
-  ctx.lineTo(-5,0);
-  ctx.stroke();
-  ctx.strokeStyle="#fff";
-  ctx.lineWidth = 40;
-    ctx.beginPath();
-    ctx.moveTo(0,100);
-    ctx.lineTo(300,100);
-    ctx.moveTo(150,0);
-    ctx.lineTo(150,200);
-    ctx.stroke();
-  ctx.strokeStyle="#c9072a";
-  ctx.lineWidth = 20;
-    ctx.beginPath();
-    ctx.moveTo(0,100);
-    ctx.lineTo(300,100);
-    ctx.moveTo(150,0);
-    ctx.lineTo(150,200);
-    ctx.stroke();
-
-  
-}
-
-void drawCanada(ctx){
+void drawCAN(ctx){
   ctx.clearRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = 'rgb(255, 0, 0)';
   ctx.fillRect(0, 0, flag_w, flag_h);
@@ -406,7 +313,212 @@ void drawCanada(ctx){
   ctx.fillStyle = 'rgb(255, 0, 0)';
   ctx.fill();
   }
+void drawJPN(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  //白色
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  // 紅心
+  ctx.beginPath();
+  ctx.arc(circle_x_jan, circle_y_jan, flag_w / 6, 0, Math.pi * 2, true);
+  ctx.closePath();
+  // 填色設為紅色
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fill();
+}
+void drawFRA(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  // 紅色  
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fillRect(200, 0, flag_w/3, flag_h);
+  //白色
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 0, flag_w/3, flag_h);
+  //藍色
+  ctx.fillStyle = 'rgb(0,0,100)';
+  ctx.fillRect(0, 0, flag_w/3, flag_h);
+}
+void drawRUS(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  // 紅色  
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fillRect(0, 133.33, flag_w, flag_h/3);
+  //白色
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, flag_w, flag_h/3);
+  //藍色
+  ctx.fillStyle = 'rgb(0,0,100)';
+  ctx.fillRect(0, 66.67, flag_w, flag_h/3);
+}
+void drawUK(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  //填藍底
+  ctx.fillStyle = 'rgb(0,0,100)';
+  ctx.fillRect(0, 0, flag_w_uk, flag_h_uk);
+  //白交叉線
+  ctx.strokeStyle="#fff";
+  ctx.lineWidth = 30;
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  ctx.lineTo(300,150);
+  ctx.moveTo(0,150);
+  ctx.lineTo(300,0);
+  ctx.stroke();
+  //左下紅線
+  ctx.strokeStyle="rgb(200,0,0)";
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(0,155);
+  ctx.lineTo(150,80);
+  ctx.stroke();
+  //右上紅線
+  ctx.strokeStyle="rgb(200,0,0)";
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(150,70);
+  ctx.lineTo(295,-5);
+  ctx.stroke();
+  //右下紅線
+  ctx.strokeStyle="rgb(200,0,0)";
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(150,70);
+  ctx.lineTo(305,145);
+  ctx.stroke();
+  //左上紅線
+  ctx.strokeStyle="rgb(200,0,0)";
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(150,80);
+  ctx.lineTo(-5,5);
+  ctx.stroke();
+  //白橫線與直線
+  ctx.strokeStyle="#fff";
+  ctx.lineWidth = 50;
+  ctx.beginPath();
+  ctx.moveTo(0,75);
+  ctx.lineTo(300,75);
+  ctx.moveTo(150,0);
+  ctx.lineTo(150,150);
+  ctx.stroke();
+  //紅線
+  ctx.strokeStyle="rgb(200,0,0)";
+  ctx.lineWidth = 30;
+  ctx.beginPath();
+  ctx.moveTo(0,75);
+  ctx.lineTo(300,75);
+  ctx.moveTo(150,0);
+  ctx.lineTo(150,150);
+  ctx.stroke();
+  //白底
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 150, flag_w, flag_h/4);
+    
+}
+void drawDEU(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  // 紅色  
+  ctx.fillStyle = 'rgb(210,0,0)';
+  ctx.fillRect(0, 66.67, flag_w, flag_h/3);
+  //黃色
+  ctx.fillStyle = 'rgb(260,180,0)';
+  ctx.fillRect(0, 133.33, flag_w, flag_h/3);
+  //黑色
+  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillRect(0, 0, flag_w, flag_h/3);
+}
+void drawNLD(ctx){
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  // 紅色  
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fillRect(0, 0, flag_w, flag_h/3);
+  //白色
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 66.67, flag_w, flag_h/3);
+  //藍色
+  ctx.fillStyle = 'rgb(0,0,100)';
+  ctx.fillRect(0, 133.33, flag_w, flag_h/3);
+}
+
+void drawUSA(ctx){
+  // 紅色
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  //藍色
+  ctx.fillStyle='rgb(0,0,100)';
+  ctx.fillRect(0, 0, flag_w/2, flag_h/1.8);
+  //白色
+  ctx.fillStyle='rgb(255,255,255)';
+  ctx.fillRect(flag_w/2,flag_h/13,flag_w,flag_h/13);
+    ctx.fillStyle='rgb(255,255,255)';
+  ctx.fillRect(flag_w/2,flag_h/4.3,flag_w,flag_h/13);
+     ctx.fillStyle='rgb(255,255,255)';
+  ctx.fillRect(flag_w/2,flag_h/2.6,flag_w,flag_h/13);
+     ctx.fillStyle='rgb(255,255,255)';
+  ctx.fillRect(0,flag_h/1.857,flag_w,flag_h/13);
+     ctx.fillStyle='rgb(255,255,255)';
+  ctx.fillRect(0,flag_h/1.4444,flag_w,flag_h/13);
+       ctx.fillStyle='rgb(255,255,255)';
+  ctx.fillRect(0,flag_h/1.1818,flag_w,flag_h/13);
+  //星星
+  num b = flag_h/18;
+  ctx.font = "10px Arial";
+  ctx.strokeStyle = 'rgb(255, 255, 255)';
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36,12);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*2);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*3);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*4);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*5);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*6);
+  ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*7);
+   ctx.strokeText("✮      ✮      ✮      ✮      ✮", flag_w / 14, b*8);
+   ctx.strokeText("✮      ✮      ✮      ✮      ✮      ✮", flag_w / 36, b*9);
+}
+
+void drawROC(ctx){
+  // 先畫滿地紅
+  ctx.clearRect(0, 0, flag_w, flag_h);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 0, flag_w, flag_h);
+  // 再畫青天
+  ctx.fillStyle = 'rgb(0, 0, 150)';
+  ctx.fillRect(0, 0, flag_w / 2, flag_h / 2);
+  // 畫十二道光芒白日
+  ctx.beginPath();
+  num star_radius = flag_w / 8;
+  num angle = 0;
+  for (int i = 0; i < 25; i++) {
+    angle += 5 * Math.pi * 2 / 12;
+    num toX = circle_x + Math.cos(angle) * star_radius;
+    num toY = circle_y + Math.sin(angle) * star_radius;
+    // 只有 i 為 0 時移動到 toX, toY, 其餘都進行 lineTo
+    if (i != 0)
+      ctx.lineTo(toX, toY);
+    else
+      ctx.moveTo(toX, toY);
+  }
+  ctx.closePath();
+  // 將填色設為白色
+  ctx.fillStyle = '#fff';
+  ctx.fill();
+  // 白日:藍圈
+  ctx.beginPath();
+  ctx.arc(circle_x, circle_y, flag_w * 17 / 240, 0, Math.pi * 2, true);
+  ctx.closePath();
+  // 填色設為藍色
+  ctx.fillStyle = 'rgb(0, 0, 149)';
+  ctx.fill();
+  // 白日:白心
+  ctx.beginPath();
+  ctx.arc(circle_x, circle_y, flag_w / 16, 0, Math.pi * 2, true);
+  ctx.closePath();
+  // 填色設為白色
+  ctx.fillStyle = '#fff';
+  ctx.fill();
+}
 
 void clearCanvas(){
   ctx.clearRect(0, 0, flag_w, flag_h);
+}
 }
